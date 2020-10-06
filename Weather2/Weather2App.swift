4227fae4e304544622bf4007213c633f49124d4d
Nick Cracchiolo <cracchiolonick@outlook.com>
2020-10-06 13:23:46 -0700

    Initial Commit

---
+++ Weather2/Weather2App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather2App.swift
+//  Weather2
+//
+//  Created by Nicholas Cracchiolo on 10/6/20.
+//
+
+import SwiftUI
+
+@main
+struct Weather2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
